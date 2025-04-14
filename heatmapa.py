63b77b98a96399f281a1be250ba7ef4ef4d7efea
AdamN3wTS <adamjanus2004@gmail.com>
2025-04-14 21:53:16 +0200

    edited heatmap

--- heatmapa.py
+++ heatmapa.py
@@ -1,12 +1,24 @@
-import pandas as  pd
+import pandas as pd
 import seaborn as sns
 import os
 import matplotlib.pyplot as plt
-os.makedirs("img",exist_ok=True)
-df= pd.read_csv("Data.csv")
-correlation = df[['VRAM (GB)', 'TGP (W)', 'GPU Score', 'MSRP ($)', 'Steam Normalized %']].corr()
 
-sns.heatmap(correlation, annot=True, cmap='coolwarm')
+
+os.makedirs("img", exist_ok=True)
+
+
+df = pd.read_csv("Data.csv")
+
+cechy = ['VRAM (GB)', 'TGP (W)', 'GPU Score', 'MSRP ($)', 'Steam Normalized %']
+correlation = df[cechy].corr()
+
+
+plt.figure(figsize=(10, 8))  
+sns.set(font_scale=1.1)      
+sns.heatmap(correlation, annot=True, fmt=".2f", cmap='coolwarm', square=True,
+            cbar_kws={"shrink": 0.8}, linewidths=0.5, linecolor='gray')
+
 plt.title('Macierz korelacji cech kart graficznych')
-plt.savefig("img/heatmap.png")
+plt.tight_layout()  
+plt.savefig("img/heatmapv2.png")
 plt.close()
